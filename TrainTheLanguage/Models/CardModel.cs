15875d00f6d44fb78f745a93a7a83e0579661809
ir h <brokzeppelin@gmail.com>
2024-04-06 05:02:53 +0200

    Add project files.

---
+++ TrainTheLanguage/Models/CardModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TrainTheLanguage.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public int Progress { get; set; }
+        public string Word { get; set; }
+        public string Definition { get; set; }
+        public string Example { get; set; }
+        public DateTime NextReview { get; set; }
+
+        public Card()
+        {
+
+        }
+
+    }
+}
